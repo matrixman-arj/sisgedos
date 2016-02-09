8b79d9d8ce67f07467f2b281e6cd9ef739cb0997
Nill Music <Nill Music@NillMusic-PC>
2016-02-09 21:57:54 -0200

    Mudança na mensagem de saída.

--- br.mil.eb.decex.ati/Sisgedos/src/main/java/br/mil/eb/decex/ati/sisgedos/servico/CadastroProdutoService.java
+++ br.mil.eb.decex.ati/Sisgedos/src/main/java/br/mil/eb/decex/ati/sisgedos/servico/CadastroProdutoService.java
@@ -20,7 +20,7 @@ public class CadastroProdutoService implements Serializable {
 		Produto produtoExistente = produtos.porSku(produto.getSku());
 		
 		if (produtoExistente != null && !produtoExistente.equals(produto)){
-			throw new NegocioException("Já existe um produto com o SKU informado");
+			throw new NegocioException("Já existe um produto com o SKU informado!");
 		}
 		
 		return produtos.guardar(produto);
